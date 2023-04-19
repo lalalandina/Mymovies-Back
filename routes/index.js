eabf03af120ce3c9e1aa2909ebf9e0e8204f0480
Sarah <lalandesarah@gmail.com>
2023-04-19 12:32:43 +0200

    test

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const MyApi = process.env.OWM_API_KEY
 
 module.exports = router;
 
 router.get('/movies', (req, res) => {
-    fetch('https://api.themoviedb.org/3/discover/movie?api_key=e5ccd97e4758610cc274a9961e8dece6')
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${MyApi}`)
       .then(response => response.json())
       .then(data => {
         res.json({ movies : data.results });
